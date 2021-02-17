851f6e4e4e01f8a97accc9d198c2691d9d313623
BKBetz <brandonbetz@live.nl>
2021-02-17 16:22:13 +0100

    removed useless code

--- P1/code/perceptronnetwork.py
+++ P1/code/perceptronnetwork.py
@@ -13,25 +13,7 @@ class PerceptronNetwork:
         output = self.layers[n].layer_output(inputs)
         # if n is smaller than len(layers)-1. it means we are still not at the end of the network.
         if n < len(self.layers)-1:
-            # """
-            #     get the amount of perceptrons for the next layer.
-            #     so we can send the output of the previous layer to the next layer.
-            #
-            #     example:
-            #     if layer 1 has 4 perceptrons and the output of those 4 (and therefore the output of the layer) is [1,0,0,1]
-            #     and layer 2 had 3 perceptrons
-            #     the input for layer 2 should be [[1,0,0,1],[1,0,0,1],[1,0,0,1]]
-            # """
-            # next_layer = self.layers[n+1].get_layer_length()
-            #
-            # new_input = []
-            #
-            # for x in range(0, next_layer):
-            #     new_input.append(output)
-
-            # change the current input to the new input so it can be used for the next layer
             new_inputs = output
-
             # loop again
             return self.feed_forward(new_inputs, n+1)
 
