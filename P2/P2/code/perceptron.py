80cb7bac8ee065febf479d92b7d51d76d547e297
BKBetz <brandonbetz@live.nl>
2021-03-07 17:59:33 +0100

    P2 herkansing

--- P2/P2/code/perceptron.py
+++ P2/P2/code/perceptron.py
@@ -22,52 +22,58 @@ class Perceptron:
         1 if sum(wi * xi) + b >= 0
     """
 
-    def __init__(self, weights: list, bias: float):
+    def __init__(self, weights: list, bias: float, lr: float):
         self.weights = weights
         self.bias = bias
         self.error_list = []
+        self.lr = lr
         # score for looping
         self.score = 0
 
     # use the inputs given by the layers to give an output
-    def activationfunc(self, inputs: list):
-        sum = 0
-        # lists should be same length
-        if len(inputs) == len(self.weights):
-            for x in range(0, len(inputs)):
-                if inputs[x] == 1:
-                    # add wn*xn to sum
-                    sum += inputs[x] * self.weights[x]
-
+    def activationfunc(self, sum: float):
         # the formula is w1*x1 w2*x2...wn*xn + bias. The sum is the wn*xn so we only add the bias
-        if sum + self.bias < 0:
+        if sum < 0:
             return 0
         else:
             return 1
 
-    def update(self, inputs: list, target: int, learning_rule: float):
+    def output(self, inputs: list):
+        # lists should be same length
+        sum = 0
+        if len(inputs) == len(self.weights):
+            for x in range(0, len(inputs)):
+                # add wn*xn to sum
+                sum += inputs[x] * self.weights[x]
+
+        total = sum + self.bias
+        output = self.activationfunc(total)
+
+        return output
+
+    def update(self, inputs: list, target: int):
         # if a update isn't necessary (prediction is already correct) add 1 to score
-        if self.activationfunc(inputs) == target:
+        if self.output(inputs) == target:
             self.score += 1
         else:
-            lr = learning_rule
-            output = self.activationfunc(inputs)
+            output = self.output(inputs)
             # error = target - output
             e = target - output
-            self.error_list.append(e)
+            # MSE = de som van alle errors^2 delen door lengte van de lijst. we kwadrateren de error dus en zetten deze in de lijst
+            self.error_list.append(e**2)
             # delta bias = learning rate * error
-            db = lr * e
+            db = self.lr * e
             # new bias = old bias + delta bias
             self.bias = self.bias + db
             for x in range(0, len(inputs)):
                 # delta weight[x] = learning rate * error * input[x]
-                dw = lr * e * inputs[x]
+                dw = self.lr * e * inputs[x]
                 # new weight[x] = old weight[x] * delta weight[x]
                 self.weights[x] = self.weights[x] + dw
 
     def error(self, e: list):
         if len(e) > 0:
-            return sum(e)**2 / len(e)
+            return sum(e) / len(e)
         else:
             return 0
 
